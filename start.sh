117725220ca91c730bfd6f3233a0a5a8a3086df5
Guillaume Rémy <csiremy.pro@gmail.com>
2023-06-06 06:57:39 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 PORT="${1:-8000}"
 
-python manage.py migrate
+python3 manage.py migrate
 echo "server running at port $PORT"
 echo "Author = $AUTHOR"
-python manage.py runserver 0.0.0.0:$PORT
+python3 manage.py runserver 0.0.0.0:$PORT
 
